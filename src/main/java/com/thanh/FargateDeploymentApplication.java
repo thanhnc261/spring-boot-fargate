7e5fdc4decfd888f3a8a38fcdcf645810abff909
thanhnguyen <thanhnc26@gmail.com>
2021-07-29 22:39:38 +0700

    Init spring boot project

---
+++ src/main/java/com/thanh/FargateDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.thanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FargateDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FargateDeploymentApplication.class, args);
+	}
+
+}
